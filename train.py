8e95f0ebc82a95b679477519aecfd26745fdb095
parthratra11 <parthratra11@gmail.com>
2024-08-28 02:48:52 +0530

    Garbage detection model done for videos - More training required

---
+++ train.py
@@ -0,0 +1,25 @@
+from ultralytics import YOLO
+import torch, os
+
+if __name__ == '__main__':
+    # Clear GPU cache
+    torch.cuda.empty_cache()
+
+    # Load a smaller model
+    model = YOLO('yolov8n.pt')  # use a smaller model like 'yolov8n.pt'
+
+    # Train the model with reduced batch size and image size
+    # FOR CPU
+    model.train(
+        # data=os.path.join(os.getcwd(), 'data.yaml'), 
+        data = 'Workspace/Datasets/d1/data.yaml',
+        epochs=50, 
+        imgsz=320,  # reduced image size
+        batch=4,  # reduced batch size
+        # half=True  # use mixed precision if supported
+        device = 'cpu',
+        workers = 8,
+        cache = True,
+    )
+
+print(os.path.join(os.getcwd(), 'data.yaml'))
