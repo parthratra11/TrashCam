a5b2658f8824ddc914de083e24d36c3947ed43b2
parthratra11 <parthratra11@gmail.com>
2024-09-20 08:38:17 +0530

    Components seperated from the HomePage

---
+++ Dashboard/admin/app/components/Homepage/invalidReports.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const InvalidReports = () => {
+  return (
+    <div className=" m-1 card card-body bg-primary h-4 flex justify-center items-center">
+      <h2 className="text-xl font-roboto text-gray-100 font-semibold">
+        Invalid Reports
+      </h2>
+    </div>
+  );
+};
+
+export default InvalidReports;
--- Dashboard/admin/app/page.tsx
+++ Dashboard/admin/app/page.tsx
@@ -1,10 +1,18 @@
 import Image from "next/image";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import {
-  faBell,
-  faLocationDot,
-  faSearch,
-} from "@fortawesome/free-solid-svg-icons";
+import { faLocationDot } from "@fortawesome/free-solid-svg-icons";
+import UrbanEcoLogo from "./components/urbanEcoLogo";
+import SearchBox from "./components/Homepage/searchBox";
+import LocationDropdown from "./components/Homepage/locationDropdown";
+import NotificationIcon from "./components/Homepage/notificationIcon";
+import DroneInspection from "./components/Homepage/droneInspection";
+import LocationMap from "./components/Homepage/locationMap";
+import HeatMap from "./components/Homepage/heatMap";
+import ViewReport from "./components/Homepage/viewReport";
+import TotalReports from "./components/Homepage/totalReports";
+import InvalidReports from "./components/Homepage/invalidReports";
+import ResolvedReports from "./components/Homepage/resolvedReports";
+import PendingReports from "./components/Homepage/pendingReports";
 
 const Dashboard = () => {
   return (
@@ -12,46 +20,10 @@ const Dashboard = () => {
       <div className="bg-base-100 p-5">
         {/* HEADER */}
         <header className="navbar bg-base-200 shadow-lg flex justify-between items-center">
-          {/* UrbanEco LOGO */}
-          <div>
-            <a className="btn btn-ghost normal-case text-xl">UrbanEco</a>
-          </div>
-
-          {/* SEARCH BOX */}
-          <div className="flex-grow flex items-center mx-4 pl-4 pr-4">
-            <div className="w-full form-control flex flex-row relative">
-              <input
-                type="text"
-                placeholder="Search"
-                className="input input-bordered w-full h-10 ml-15 mr-15"
-              />
-              <div className="absolute flex items-center justify-center right-0 top-0 bottom-0 px-4">
-                <FontAwesomeIcon
-                  icon={faSearch}
-                  color="gray"
-                  className="ml-2"
-                />
-              </div>
-            </div>
-          </div>
-
-          {/* LOCATION DROPDOWN */}
-          <div className="flex-none gap-2 flex items-center">
-            <div className="dropdown menu-dropdown ml-auto">
-              <FontAwesomeIcon icon={faLocationDot} className="mr-1" />
-              <select className="select select-bordered font-semibold btn ml-2 flex-shrink-0 w-40">
-                <option className="font-semibold">Saket, Delhi</option>
-                <option className="font-semibold">Live Location</option>
-              </select>
-            </div>
-          </div>
-
-          {/* NOTIFICATIONS */}
-          <div className="ml-2 mr-1">
-            <button className="btn btn-circle bg-base-100">
-              <FontAwesomeIcon icon={faBell} className="text-xl" />
-            </button>
-          </div>
+          <UrbanEcoLogo />
+          <SearchBox />
+          <LocationDropdown />
+          <NotificationIcon />
         </header>
 
         {/* MAIN-DASHBOARD */}
@@ -60,27 +32,12 @@ const Dashboard = () => {
           <div className="lg:col-span-2">
             {/* REPORTS OVERVIEW */}
             <div className="grid grid-cols-2 md:grid-cols-2 gap-1 border border-black">
-              <div className=" m-1 card card-body bg-primary h-4 flex justify-center items-center">
-                <h2 className="text-xl font-roboto text-gray-100 font-semibold">
-                  Total Reports
-                </h2>
-              </div>
-              <div className=" m-1 card card-body bg-primary h-4 flex justify-center items-center">
-                <h2 className="text-xl font-roboto text-gray-100 font-semibold">
-                  Invalid Reports
-                </h2>
-              </div>
-              <div className=" m-1 card card-body bg-primary h-4 flex justify-center items-center">
-                <h2 className="text-xl font-roboto text-gray-100 font-semibold">
-                  Resolved Reports
-                </h2>
-              </div>
-              <div className=" m-1 card card-body bg-primary h-4 flex justify-center items-center">
-                <h2 className="text-xl font-roboto text-gray-100 font-semibold">
-                  Pending Reports
-                </h2>
-              </div>
+              <TotalReports />
+              <InvalidReports />
+              <ResolvedReports />
+              <PendingReports />
             </div>
+
             {/* REPORTS LIST */}
             <div className="border border-black p-1">
               Reports
@@ -113,25 +70,10 @@ const Dashboard = () => {
           </div>
           {/* COLUMN 2 */}
           <div className="border border-black flex flex-col justify-between items-center p-1">
-            {/* VIEW REPORT */}
-            <div className="border border-black w-full font-roboto font-semibold pr-2 pl-4 card flex flex-row p-2 justify-between items-center">
-              Hotspot Areas
-              <button className="btn btn-ghost bg-base-200">View Report</button>
-            </div>
-            {/* HEATMAP */}
-            <div className="card border border-black w-full h-full mt-3 flex justify-center items-center">
-              Heatmap
-            </div>
-            {/* LOCATION MAP */}
-            <div className="card border border-black w-full h-full m-3 flex justify-center items-center">
-              Location Map
-            </div>
-            {/* DRONE INSPECTION */}
-            <div className="card w-full">
-              <button className="btn btn-ghost text-lg bg-primary text-white font-roboto">
-                Drone Inspection
-              </button>
-            </div>
+            <ViewReport />
+            <HeatMap />
+            <LocationMap />
+            <DroneInspection />
           </div>
         </div>
       </div>
